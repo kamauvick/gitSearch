bdded76222ca22cec2a0832ac6bf8a6b2444c55d
kamauvick <kamauvick@outlook.com>
2019-08-23 15:51:49 +0300

    Add a searchbar to search for users and repositories.
    Include event emitters to emit the searched term to the other components.

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-search-user',
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-user.component.css']
 })
 export class SearchUserComponent implements OnInit {
+  searchTerm: string;
+  @Output() searchEmmiter = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {
+  }
+
+  emmitUser() {
+  this.searchEmmiter.emit(this.searchTerm);
+  // console.log(this.searchUser);
+  }
 
   ngOnInit() {
   }
