e98a9f0be4428cf2b08c438581deb9fa7a4f84e0
kamauvick <kamauvick@outlook.com>
2019-08-23 15:54:08 +0300

    Add a component to display user repositories.

--- src/app/user-repo-list/user-repo-list.component.ts
+++ src/app/user-repo-list/user-repo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-repo-list',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRepoListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  getSearchedRepos(searchTerm) {
   }
 
 }
