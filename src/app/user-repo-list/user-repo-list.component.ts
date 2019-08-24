b4639c22adb90c269d12675507ae9ed128e883cb
kamauvick <kamauvick@outlook.com>
2019-08-24 21:41:23 +0300

    inject my-repo-service to the repo-list component and call methods in the service.

--- src/app/user-repo-list/user-repo-list.component.ts
+++ src/app/user-repo-list/user-repo-list.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {RepoServiceService} from '../repo-service.service';
+import {RepoModel} from '../repo-model';
 
 @Component({
   selector: 'app-user-repo-list',
@@ -6,14 +8,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-repo-list.component.css']
 })
 export class UserRepoListComponent implements OnInit {
+  myRepo: RepoModel[];
 
-  constructor() {
+  constructor(public reposerv: RepoServiceService) {
+  }
+
+  getRepo(searchTerm: string) {
+    this.reposerv.getRepo(searchTerm).subscribe(data => {
+      this.myRepo = data;
+      console.log(this.myRepo);
+    });
   }
 
   ngOnInit() {
+    this.getRepo('kamauvick');
   }
-
-  getSearchedRepos(searchTerm) {
-  }
-
 }
