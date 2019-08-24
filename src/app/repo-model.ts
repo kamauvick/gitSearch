7cfcc2f6da9dd4e701d593f25b33eddf008ee43d
kamauvick <kamauvick@outlook.com>
2019-08-24 21:34:27 +0300

    Add a repository model class.

--- src/app/repo-model.ts
+++ src/app/repo-model.ts
@@ -1,2 +1,11 @@
 export class RepoModel {
+  description: any;
+  name: any;
+  owner: any;
+  watchers: any;
+  forks: any;
+  // tslint:disable-next-line:variable-name
+  default_branch: any;
+  language: any;
+
 }
